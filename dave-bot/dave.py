e3a18b36e87f17784fc7c4dcfe2cb8a49107a9e5
relativisticpoint <hsami272@gmail.com>
2022-04-17 12:50:41 +0200

    name of the bot

---
+++ dave-bot/dave.py
@@ -0,0 +1,23 @@
+import pyautogui as dave
+import time 
+import sys 
+
+def play_a_song(name):
+    dave.hotkey('command','space') #opens the spotlight search to look for the spotify app
+    dave.write("spotify")
+    dave.press("enter")
+    time.sleep(1)
+    dave.hotkey('command','l') #uses the default spotify search shortcut
+    dave.write(name)
+    dave.press("enter")
+    time.sleep(1)
+    dave.click(x=746,y=158) #clicks on the first recommended song
+    return
+    
+if __name__=="__main__":
+    name_of_song = ""
+    for i in range(1,len(sys.argv)):
+        name_of_song += sys.argv[i] + " "
+    print("Launching Spotify and playing ", name_of_song)
+    play_a_song(name_of_song)
+
