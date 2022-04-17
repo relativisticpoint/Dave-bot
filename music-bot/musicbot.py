82e3dbee3d66a944855e2578abe5ce9263fad012
relativisticpoint <hsami272@gmail.com>
2022-04-17 12:02:49 +0200

    add a return statement instead of stopping the process

--- music-bot/musicbot.py
+++ music-bot/musicbot.py
@@ -6,22 +6,21 @@ from py import process
 import sys 
 
 def play_a_song(name):
-    # auto.click(x=541, y=865) #opens spotify
-    auto.hotkey('command','space') 
+    auto.hotkey('command','space') #opens the spotlight search to look for the spotify app
     auto.write("spotify")
     auto.press("enter")
     time.sleep(1)
-    auto.click(x=55,y=78) #va dans la barre chercher 
-    auto.click(x=704, y=31)
+    auto.click(x=55,y=78) # goes to the search bar
+    auto.click(x=704, y=31) #clicks on the cross to delete any previous search in the search bar
     auto.write(name)
     auto.press("enter")
     time.sleep(1)
-    auto.click(x=746,y=158)
-    process.exit(1)
+    auto.click(x=746,y=158) #clicks on the first recommended song
+    return
 if __name__=="__main__":
     name_of_song = ""
     for i in range(1,len(sys.argv)):
         name_of_song += sys.argv[i] + " "
-    print("looking for", name_of_song)
+    print("Launching Spotify and playing ", name_of_song)
     play_a_song(name_of_song)
 
