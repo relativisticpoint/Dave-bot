5e20f18e8bf72ec32c6033720eddfd5218f8284c
relativisticpoint <hsami272@gmail.com>
2022-04-17 12:13:30 +0200

    use of the spotify search shortcut instead of the click

--- music-bot/musicbot.py
+++ music-bot/musicbot.py
@@ -1,5 +1,3 @@
-from html.entities import name2codepoint
-from unicodedata import name
 import pyautogui as auto
 import time 
 from py import process
@@ -10,13 +8,13 @@ def play_a_song(name):
     auto.write("spotify")
     auto.press("enter")
     time.sleep(1)
-    auto.click(x=55,y=78) # goes to the search bar
-    auto.click(x=704, y=31) #clicks on the cross to delete any previous search in the search bar
+    auto.hotkey('command','l') #uses the default spotify search shortcut
     auto.write(name)
     auto.press("enter")
     time.sleep(1)
     auto.click(x=746,y=158) #clicks on the first recommended song
     return
+    
 if __name__=="__main__":
     name_of_song = ""
     for i in range(1,len(sys.argv)):
